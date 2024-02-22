f0591d077f37b60062bd22ae50a09118166ae83d
crictonn <devicejoy@gmail.com>
2024-02-22 17:35:56 +0300

    initial commit

---
+++ src/main/java/com/cherkas/vladimir/logisticstransportations/LogisticsTransportationsApplication.java
@@ -0,0 +1,13 @@
+package com.cherkas.vladimir.logisticstransportations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsTransportationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsTransportationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cherkas/vladimir/logisticstransportations/LogisticksTransportationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherkas.vladimir.logisticstransportations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticksTransportationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
