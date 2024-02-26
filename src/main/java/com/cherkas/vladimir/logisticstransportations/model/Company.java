ff34d60fa76c5d320c267e8bb42e153b383ea0f9
crictonn <devicejoy@gmail.com>
2024-02-26 14:03:53 +0300

    started to write models

---
+++ src/main/java/com/cherkas/vladimir/logisticstransportations/model/Company.java
@@ -0,0 +1,20 @@
+package com.cherkas.vladimir.logisticstransportations.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "companies")
+@Data
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+
+
+    //Тут нужен список заказов, которые взяла компания
+}
---
+++ src/main/java/com/cherkas/vladimir/logisticstransportations/model/User.java
@@ -0,0 +1,33 @@
+package com.cherkas.vladimir.logisticstransportations.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 30, nullable = false)
+    private String first_name;
+
+    @Column(name = "second_name", length = 30)
+    private String second_name;
+
+    @Column(name = "username", length = 30, nullable = false, unique = true)
+    private String username;
+
+    @Column(name ="email", length = 30, nullable = false, unique = true)
+    private String email;
+
+    @Column(name ="password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "role", length = 10, nullable = false)
+    private String role;
+
+    //Дописать лист или сет заказов конкретного пользователя
+}
